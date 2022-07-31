85ceff16b7b862e32f927fe02ecad952769c5fd0
Anurag Sen <anurag.sen@telenetgroup.be>
2022-07-31 13:11:53 +0530

    Initial

--- src/main/java/com/capstone/service/MovieService.java
+++ src/main/java/com/capstone/service/MovieService.java
@@ -64,8 +64,4 @@ public class MovieService {
 	public void removeMovie(int movieId) {
 		repo.deleteById(movieId);
 	}
-	
-//	public void removeMovie(Movie movie) {
-//		repo.delete(movie);
-//	}
 }
